35a52cc81bac9971ed2d8399054e006ec643660a
hannad4 <hannad4@tcnj.edu>
2020-03-04 13:46:58 -0500

    adding temperature characteristic placeholder

--- angleReadAttempts/rollPitchYawAngleRead/imuOutputLSM9DS1/imuOutputLSM9DS1.ino
+++ angleReadAttempts/rollPitchYawAngleRead/imuOutputLSM9DS1/imuOutputLSM9DS1.ino
@@ -13,11 +13,13 @@ float accelX, accelY, accelZ;
 float magX, magY, magZ, mag_x, mag_y;
 float gyroX, gyroY, gyroZ;
 float roll, pitch, yaw, rollF = 0, pitchF = 0, yawF = 0, posiRoll, posiPitch, posiYaw;  
+float temp = 43; 
 
 BLEService angleService("1826");
 BLEStringCharacteristic pitchBLE("78c5307a-6715-4040-bd50-d64db33e2e9e", BLERead | BLENotify, 20);
 BLEStringCharacteristic rollBLE("78c5307b-6715-4040-bd50-d64db33e2e9e", BLERead | BLENotify, 20);
 BLEStringCharacteristic yawBLE("78c5307c-6715-4040-bd50-d64db33e2e9e", BLERead | BLENotify, 20);
+BLEStringCharacteristic temperature("78c5307d-6715-4040-bd50-d64db33e2e9e", BLERead | BLENotify, 20); 
 
 void setup()
 {
@@ -39,6 +41,7 @@ void setup()
   angleService.addCharacteristic(pitchBLE); 
   angleService.addCharacteristic(rollBLE);
   angleService.addCharacteristic(yawBLE); 
+  angleService.addCharacteristic(temperature); 
  
   BLE.addService(angleService);
   rollBLE.writeValue("0"); 
@@ -76,6 +79,7 @@ void loop()
         posiRoll = rollF * -1;
         posiPitch = pitchF * -1;
         posiYaw = yawF * -1; 
+        
 
         //Serial.print("Time: ");
         //Serial.print(millis() / 1000.0);
@@ -90,6 +94,7 @@ void loop()
         rollBLE.writeValue(String(posiRoll)); 
         pitchBLE.writeValue(String(posiPitch));
         yawBLE.writeValue(String(posiYaw)); 
+        temperature.writeValue(String(temp)); 
       }
     }
   }
